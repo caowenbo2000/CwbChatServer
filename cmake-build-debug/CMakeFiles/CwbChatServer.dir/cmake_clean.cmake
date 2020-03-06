d25a78f91d00fcc399b6a8a3aa2ef66d7332ec7b
caowenbo2000 <2311694605@qq.com>
2020-03-06 15:03:21 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/CwbChatServer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CwbChatServer.dir/main.cpp.o"
+  "CwbChatServer"
+  "CwbChatServer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CwbChatServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
